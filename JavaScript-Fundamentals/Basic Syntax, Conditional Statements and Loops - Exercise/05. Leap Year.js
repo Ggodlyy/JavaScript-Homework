97735672dd0c90d6f2e8930b77bebebdcf29f1da
Ggodlyy <nikogerenski@abv.bg>
2021-05-21 17:52:37 +0300

    Create 05. Leap Year.js

---
+++ JavaScript-Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/05. Leap Year.js
@@ -0,0 +1,8 @@
+function solve(year) {
+    if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+        return 'yes';
+    } else {
+        return 'no';
+    }
+}
+console.log(solve(4));
