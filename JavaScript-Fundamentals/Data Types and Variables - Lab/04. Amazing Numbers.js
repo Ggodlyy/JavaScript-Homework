ed4511aa0c463a064bd239acc4966f3015bd3532
Ggodlyy <nikogerenski@abv.bg>
2021-05-22 14:02:28 +0300

    Update 04. Amazing Numbers.js

--- JavaScript-Fundamentals/Data Types and Variables - Lab/04. Amazing Numbers.js
+++ JavaScript-Fundamentals/Data Types and Variables - Lab/04. Amazing Numbers.js
@@ -1,4 +1,17 @@
-function solve() {
+function solve(num) {
+    let string = num.toString();
+    let sum = string
+        .split('')
+        .map(Number)
+        .reduce((acc, curr) => acc += curr, 0);
 
+
+    let result = sum.toString();
+
+    if (result.includes('9')) {
+        console.log(`${num} Amazing? True`);
+    } else {
+        console.log(`${num} Amazing? False`);
+    }
 }
-solve()
+solve(583472)
