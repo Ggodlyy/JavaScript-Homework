407f64ae4d9a08bf3a5017a18fd6555d922ca2d3
Ggodlyy <nikogerenski@abv.bg>
2021-05-22 16:50:34 +0300

    Update 01. Fruit.js

--- JavaScript-Advanced/Basic Syntax, Conditional Statements and Loops - Exercise/01. Fruit.js
+++ JavaScript-Advanced/Basic Syntax, Conditional Statements and Loops - Exercise/01. Fruit.js
@@ -1,6 +1,6 @@
 function solve(fruit, grams, pricePerKg) {
     let kilograms = grams / 1000;
     let price = kilograms * pricePerKg;
-    return `I need ${price.toFixed(2)} to buy ${kilograms.toFixed(2)} kilograms ${fruit}.`
+    return `I need $${price.toFixed(2)} to buy ${kilograms.toFixed(2)} kilograms ${fruit}.`
 }
 console.log(solve('orange', 2500, 1.80));
