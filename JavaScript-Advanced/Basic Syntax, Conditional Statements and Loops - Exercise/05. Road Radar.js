1bd693fc6ba2e4e776fc0ae7d74553fa82515bf0
Ggodlyy <nikogerenski@abv.bg>
2021-05-23 13:13:51 +0300

    Update 05. Road Radar.js

--- JavaScript-Advanced/Basic Syntax, Conditional Statements and Loops - Exercise/05. Road Radar.js
+++ JavaScript-Advanced/Basic Syntax, Conditional Statements and Loops - Exercise/05. Road Radar.js
@@ -1,4 +1,30 @@
-function solve() {
+function solve(speed, area) {
+    let speedLimits = {
+        motorway: 130,
+        interstate: 90,
+        city: 50,
+        residential: 20
+    }
 
+    if (speed <= speedLimits[area]) {
+        console.log(`Driving ${speed} km/h in a ${speedLimits[area]} zone`);
+    } else {
+        console.log(speeding(speed, speedLimits[area]));
+    }
+
+    function speeding(speed, limit) {
+        let speedDiffrence = Math.abs(speed - limit);
+        let status = '';
+
+        if (speedDiffrence <= 20) {
+            status = 'speeding';
+        } else if (speedDiffrence <= 40) {
+            status = 'excessive speeding';
+        } else if (speedDiffrence > 40) {
+            status = 'reckless driving';
+        }
+
+        return `The speed is ${speedDiffrence} km/h faster than the allowed speed of ${limit} - ${status}`
+    }
 }
-solve(40, 'city')
+solve(200, 'motorway')
