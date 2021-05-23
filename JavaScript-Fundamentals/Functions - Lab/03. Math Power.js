744a46bc2c1011515fbb71c9697501c4b4ddb95b
Ggodlyy <nikogerenski@abv.bg>
2021-05-23 14:08:16 +0300

    Update 03. Math Power.js

--- JavaScript-Fundamentals/Functions - Lab/03. Math Power.js
+++ JavaScript-Fundamentals/Functions - Lab/03. Math Power.js
@@ -1,4 +1,10 @@
 function solve(num, power) {
     return num ** power;
 }
-solve(2, 8)
+
+function otherSolution(num, power) {
+    return Math.pow(num, power);
+}
+solve(2, 8);
+
+console.log(otherSolution(2, 8))
