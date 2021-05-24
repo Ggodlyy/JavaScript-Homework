015db4b059da5d687f9dfccc35d7e3f28998d86a
Ggodlyy <nikogerenski@abv.bg>
2021-05-24 14:43:48 +0300

    Update 02. Modern Times of #(HashTag).js

--- JavaScript-Fundamentals/Text Processing - Exercise/02. Modern Times of #(HashTag).js
+++ JavaScript-Fundamentals/Text Processing - Exercise/02. Modern Times of #(HashTag).js
@@ -1,6 +1,7 @@
 function solve(text) {
     let pattern = /#[A-Za-z]+\b/g;
-    let matches = text.match(pattern);
-    return matches.join('\n');
+    let matches = text.match(pattern);   
+    matches.forEach(w => w = w.slice(1));
+    console.log(matches);
 }
 solve('Nowadays everyone uses # to tag a #special word in #socialMedia');
