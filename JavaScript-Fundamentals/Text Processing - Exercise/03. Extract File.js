35e52a996992e180855c4503629853a975d0f5ae
Ggodlyy <nikogerenski@abv.bg>
2021-05-24 14:50:42 +0300

    Update 03. Extract File.js

--- JavaScript-Fundamentals/Text Processing - Exercise/03. Extract File.js
+++ JavaScript-Fundamentals/Text Processing - Exercise/03. Extract File.js
@@ -1,9 +1,10 @@
-function solve(text) {
-    let arr = text.split('\\');
-    let fileAndExtension = arr[arr.length - 1];
-    let tokens = fileAndExtension.split('.');
+function solve(string) {
+    let startIndex = string.lastIndexOf("\\") + 1;
+    let endIndex = string.lastIndexOf('.');
+    let name = string.substring(startIndex, endIndex);
+    let extension = string.substring(endIndex + 1);
 
-    console.log(`File name: ${tokens[0]}`);
-    console.log(`File extension: ${tokens[tokens.length - 1]}`);
+    console.log(`File name: ${name}`);
+    console.log(`File extension: ${extension}`);
 }
 solve('C:\\Internal\\training-internal\\Template.pptx')
