096f5df66e24be5e283d5474357a0f344f6daac2
Ggodlyy <nikogerenski@abv.bg>
2021-05-24 16:32:13 +0300

    Update 03. Match Dates.js

--- JavaScript-Fundamentals/Regular Expressions - Lab/03. Match Dates.js
+++ JavaScript-Fundamentals/Regular Expressions - Lab/03. Match Dates.js
@@ -1,5 +1,12 @@
-function solve() {
-    let pattern = /\d{2}([\.-\/])[A-Z][a-z]{2}\1\d{4}/g;
-    
+function solve(arr) {
+    let pattern = /\b(?<day>\d{2})([\.\-\/])(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})\b/g;
+    let matches = pattern.exec(arr[0]);
+
+    while (matches !== null) {
+        console.log(`Day: ${matches.groups.day}, Month: ${matches.groups.month}, Year: ${matches.groups.year}`);
+
+        matches = pattern.exec(arr[0]);
+    }
+
 }
-solve()
+solve(["13/Jul/1928, 10-Nov-1934, , 01/Jan-1951,f 25.Dec.1937 23/09/1973, 1/Feb/2016"])
