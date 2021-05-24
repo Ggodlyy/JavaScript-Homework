970489fe46b231f1cbbf0e8d8225e1f89dbde344
Ggodlyy <nikogerenski@abv.bg>
2021-05-24 16:17:05 +0300

    Update 02. Match Phone Number.js

--- JavaScript-Fundamentals/Regular Expressions - Lab/02. Match Phone Number.js
+++ JavaScript-Fundamentals/Regular Expressions - Lab/02. Match Phone Number.js
@@ -1,6 +1,6 @@
-function solve(text) {
+function solve(arr) {
     let pattern = /\+359([ -])2\1\d{3}\1\d{4}\b/g;
-    let matches = text.match(pattern);
+    let matches = arr[0].match(pattern);
     return matches.join(', ');
 }
-console.log(solve("+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222"));
+console.log(solve(["+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222"]));
