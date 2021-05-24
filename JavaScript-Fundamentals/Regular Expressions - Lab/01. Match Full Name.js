d442c1c068129067441941e8f90e43003d181618
Ggodlyy <nikogerenski@abv.bg>
2021-05-24 15:53:56 +0300

    Update 01. Match Full Name.js

--- JavaScript-Fundamentals/Regular Expressions - Lab/01. Match Full Name.js
+++ JavaScript-Fundamentals/Regular Expressions - Lab/01. Match Full Name.js
@@ -2,6 +2,6 @@ function solve(text) {
     let pattern = /\b[A-Z][a-z]{1,} [A-Z][a-z]{1,}\b/g;
     let matches = text.match(pattern);
 
-    console.log(matches.join(' '));
+    return matches.join(' ');
 }
 solve("Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov")
