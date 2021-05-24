0fd8a06b707b960693128aa685e889282d88b5dd
Ggodlyy <nikogerenski@abv.bg>
2021-05-24 13:51:05 +0300

    Update 04. Count String Occurrences.js

--- JavaScript-Fundamentals/Text Processing - Lab/04. Count String Occurrences.js
+++ JavaScript-Fundamentals/Text Processing - Lab/04. Count String Occurrences.js
@@ -1,4 +1,21 @@
-function solve() {
+function otherSolve(text, word) {
+    let arr = text.split(' ');
+    let count = 0;
 
+    for (let input of arr) {
+        if (input === word) {
+            count++;
+        }
+    }
+
+    return count;
 }
-solve()
+
+function moreSolve(text, word) {
+    let arr = text.split(' ').filter((el) => el === word);
+    return arr.length;
+}
+
+solve('This is a word and it also is a sentence', 'is');
+otherSolve('This is a word and it also is a sentence', 'is');
+moreSolve('This is a word and it also is a sentence', 'is');
