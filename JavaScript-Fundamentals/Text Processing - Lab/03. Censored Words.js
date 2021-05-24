0c5a11f5639f5de4cd23edfed5cae36b97b40c67
Ggodlyy <nikogerenski@abv.bg>
2021-05-24 13:22:09 +0300

    Update 03. Censored Words

--- JavaScript-Fundamentals/Text Processing - Lab/03. Censored Words.js
+++ JavaScript-Fundamentals/Text Processing - Lab/03. Censored Words.js
@@ -3,4 +3,16 @@ function solve(text, word) {
     let result = text.replace(pattern, '*'.repeat(word.length));
     return result;
 }
+
+function otherSolve(text, word) {
+    let censored = text.replace(word, '*'.repeat(word.length));
+
+    while (censored.includes(word)) {
+        censored = censored.replace(word, '*'.repeat(word.length));
+    }
+
+    console.log(censored);
+}
+
 solve('A small sentence with some words', 'small');
+otherSolve('A small sentence with some words', 'small');
