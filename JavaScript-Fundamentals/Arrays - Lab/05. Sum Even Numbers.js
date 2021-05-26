f5824e4a65cc66d401e66b70d029fae28554ecaf
Ggodlyy <nikogerenski@abv.bg>
2021-05-26 15:48:36 +0300

    Update 05. Sum Even Numbers.js

--- JavaScript-Fundamentals/Arrays - Lab/05. Sum Even Numbers.js
+++ JavaScript-Fundamentals/Arrays - Lab/05. Sum Even Numbers.js
@@ -5,4 +5,16 @@ function solve(arr) {
         .reduce((acc, curr) => acc += curr, 0);
 }
 
+function otherSolution(arr) {
+    let sum = 0;
+
+    for (let input of arr) {
+        if (Number(input) % 2 === 0) {
+            sum += Number(input);
+        }
+    }
+
+    return sum;
+}
+
 console.log(solve(['1', '2', '3', '4', '5', '6']));
