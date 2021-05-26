cd031f2c3b487db93dab4a78061d0a3ec69e0c20
Ggodlyy <nikogerenski@abv.bg>
2021-05-26 16:34:04 +0300

    Create 08. Condense Array to Number.js

---
+++ JavaScript-Fundamentals/Arrays - Lab/08. Condense Array to Number.js
@@ -0,0 +1,36 @@
+function solve(arr) {
+    while (arr.length > 1) {
+        let newArr = [];
+
+        for (let i = 0; i < arr.length; i++) {
+            let firstEl = arr[i];
+            let secondEl = arr[i + 1];
+
+            if (secondEl === undefined) {
+                break;
+            }
+
+            newArr[i] = firstEl + secondEl;
+        }
+
+        arr = newArr;
+    }
+
+    return arr[0];
+}
+
+function otherSolution(arr) {
+    while (arr.length > 1) {
+        for (let i = 0; i < arr.length - 1; i++) {
+            let num = arr[i];
+            let nextNum = arr[i + 1];
+            arr[i] = num + nextNum;
+        }
+        arr.pop();
+    }
+
+    return arr[0];
+}
+
+console.log(solve([2, 10, 3]));
+console.log(otherSolution([2, 10, 3]));
