346285477a52435b51444a1d2778a8051adbb294
Ggodlyy <nikogerenski@abv.bg>
2021-05-27 20:05:57 +0300

    Create 10. Build a Wall

---
+++ JavaScript-Fundamentals/Arrays Advanced - Exercise/10. Build a Wall.js
@@ -0,0 +1,28 @@
+function solve(arr) {
+    let limit = 30;
+    let concreteInYards = 195;
+    let totalConcreteInYards = [];
+    let pricePerYard = 1900;
+
+    while (arr.length > 0) {
+        let concreteAmountInADay = 0;
+
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === limit) {
+                continue;
+            } else {
+                arr[i]++;
+                concreteAmountInADay += concreteInYards;
+            }
+        }
+
+        let upgradedArr = arr.filter(el => el !== limit);
+        arr = upgradedArr;
+        totalConcreteInYards.push(concreteAmountInADay);
+    }
+
+    let totalPrice = totalConcreteInYards.reduce((acc, curr) => acc += curr * pricePerYard, 0);
+    console.log(totalConcreteInYards.join(', '));
+    console.log(`${totalPrice} pesos`);
+}
+solve([21, 25, 28])
