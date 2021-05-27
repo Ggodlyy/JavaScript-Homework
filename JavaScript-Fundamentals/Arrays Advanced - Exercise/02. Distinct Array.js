18bfd6b205e89192a53502f111430e1f0a393dc6
Ggodlyy <nikogerenski@abv.bg>
2021-05-27 17:42:50 +0300

    Create 02. Distinct Array.js

---
+++ JavaScript-Fundamentals/Arrays Advanced - Exercise/02. Distinct Array.js
@@ -0,0 +1,15 @@
+function solve(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                arr[j] = '0';
+            }
+        }
+    }
+
+    let result = arr.filter(e => e !== '0');
+
+    return result.join(' ');
+}
+
+console.log(solve([20, 8, 12, 13, 4, 4, 8, 5]));
