c8c699d5d5c66d5c24f3c7fe08caa557bb926f35
Ggodlyy <nikogerenski@abv.bg>
2021-05-27 14:43:50 +0300

    Update 04. Non-Decreasing Subsequence.js

--- JavaScript-Fundamentals/Arrays - More Exercise/04. Non-Decreasing Subsequence.js
+++ JavaScript-Fundamentals/Arrays - More Exercise/04. Non-Decreasing Subsequence.js
@@ -14,17 +14,5 @@ function solve(arr) {
     return result.join(' ');
 }
 
-function otherSolution(arr) {
-    let bigNum = 0;
-    let result = arr.filter(el => {
-        if (el >= bigNum) {
-            bigNum = el;
-            return el
-        }
-    });
-
-    return result.join(' ');
-}
-
 console.log(solve([1, 3, 8, 4, 10, 12, 3, 2, 24]));
-console.log(otherSolution([1, 3, 8, 4, 10, 12, 3, 2, 24]))
+
