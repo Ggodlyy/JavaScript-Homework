9f4124c7002614b8849223c27950edc1cb927190
Ggodlyy <nikogerenski@abv.bg>
2021-05-27 17:44:18 +0300

    Update 02. Distinct Array.js

--- JavaScript-Fundamentals/Arrays Advanced - Exercise/02. Distinct Array.js
+++ JavaScript-Fundamentals/Arrays Advanced - Exercise/02. Distinct Array.js
@@ -1,5 +1,9 @@
 function solve(arr) {
     for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '0') {
+            continue;
+        }
+        
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[i] === arr[j]) {
                 arr[j] = '0';
