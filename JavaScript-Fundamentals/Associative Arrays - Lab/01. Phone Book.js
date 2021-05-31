cf71e8308adae442a957561defa17760d91e6f74
Ggodlyy <nikogerenski@abv.bg>
2021-05-31 20:14:30 +0300

    Create 01. Phone Book.js

---
+++ JavaScript-Fundamentals/Associative Arrays - Lab/01. Phone Book.js
@@ -0,0 +1,18 @@
+function solve(arr) {
+    let obj = {};
+
+    for (let input of arr) {
+        let [name, phoneNumber] = input.split(' ');
+
+        obj[name] = parseInt(phoneNumber);
+    }
+
+    for (let key in obj) {
+        console.log(`${key} -> ${obj[key]}`);
+    }
+}
+solve(['Tim 0834212554',
+    'Peter 0877547887',
+    'Bill 0896543112',
+    'Tim 0876566344']
+)
