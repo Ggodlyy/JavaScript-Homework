c5f653394553eff347e794c8004bf2804030c64b
Ggodlyy <nikogerenski@abv.bg>
2021-06-01 16:04:47 +0300

    Create 03. City Taxes.js

---
+++ JavaScript-Advanced/Objects and Composition - Lab/03. City Taxes.js
@@ -0,0 +1,42 @@
+function solve() {
+    const city =
+        cityTaxes('Tortuga',
+            7000,
+            15000);
+    console.log(city);
+
+    city.collectTaxes();
+    console.log(city.treasury);
+    city.applyGrowth(5);
+    console.log(city.population);
+
+
+    function cityTaxes(name, population, treasury) {
+        let collectTaxes = function () {
+            this.treasury += (this.population * this.taxRate);
+        }
+
+        let applyGrowth = function (percentige) {
+            percentige /= 100;
+            this.population += (this.population * percentige);
+        }
+
+        let applyRecession = function (percentige) {
+            percentige /= 100;
+            this.treasury -= (this.treasury * percentige);
+        }
+
+        return {
+            name,
+            population,
+            treasury,
+            taxRate: 10,
+            collectTaxes,
+            applyGrowth,
+            applyRecession
+        }
+    }
+}
+
+
+solve()
