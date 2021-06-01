66767820f27a1febf56ff54fb3475b70691116b0
Ggodlyy <nikogerenski@abv.bg>
2021-06-01 20:14:03 +0300

    Update 10. Heroes.js

--- JavaScript-Advanced/Objects and Composition - Exercise/10. Heroes.js
+++ JavaScript-Advanced/Objects and Composition - Exercise/10. Heroes.js
@@ -37,6 +37,47 @@ function solve() {
     };
 }
 
+function otherSolution() {
+    let canCast = (obj) => ({
+        cast: (spell) => {
+            console.log(`${obj.name} cast ${spell}`);
+            obj.mana--;
+        }
+    })
+
+    let canFight = (obj) => ({
+        fight: () => {
+            console.log(`${obj.name} slashes at the foe!`);
+            obj.stamina--;
+        }
+    })
+
+    let fighter = (name) => {
+        let obj = {
+            name,
+            health: 100,
+            stamina: 100
+        }
+
+        return Object.assign(obj, canFight(obj));
+    }
+
+    let mage = (name) => {
+        let obj = {
+            name,
+            health: 100,
+            mana: 100
+        }
+
+        return Object.assign(obj, canCast(obj));
+    }
+
+    return {
+        fighter,
+        mage
+    }
+}
+
 
 let create = solve();
 const scorcher = create.mage("Scorcher");
