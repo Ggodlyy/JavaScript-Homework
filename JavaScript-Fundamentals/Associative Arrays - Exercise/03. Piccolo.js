8064099e3766ed1c8fab459fdde96fe755d74e56
Ggodlyy <nikogerenski@abv.bg>
2021-06-01 14:58:08 +0300

    Update 03. Piccolo.js

--- JavaScript-Fundamentals/Associative Arrays - Exercise/03. Piccolo.js
+++ JavaScript-Fundamentals/Associative Arrays - Exercise/03. Piccolo.js
@@ -1,31 +1,29 @@
 function solve(arr) {
-    let parkingLot = {};
+    let cars = [];
 
-    for (let input of arr) {
-        let [direction, car] = input.split(', ');
+    for (let item of arr) {
+        let [direction, carNumber] = item.split(", ");
 
-        if (direction === 'IN') {
-            let carNumber = car
-                .split('')
-                .filter(el => !isNaN(el))
-                .join('');
-
-            parkingLot[car] = Number(carNumber);
-        } else {
-            if (parkingLot.hasOwnProperty(car)) {
-                delete parkingLot[car];
+        if (direction === "IN") {
+            if (!cars.includes(carNumber)) {
+                cars.push(carNumber);
+            }
+        } else if (direction === "OUT") {
+            if (cars.includes(carNumber)) {
+                let index = cars.indexOf(carNumber);
+                cars.splice(index, 1)
             }
         }
     }
 
-    let result = Object.entries(parkingLot).sort((a, b) => a[1] - b[1]);
+    let sorted = cars.sort();
 
-    if (result.length > 0) {
-        for (let arr of result) {
-            console.log(arr[0]);
+    if (cars.length > 0) {
+        for (let number of sorted) {
+            console.log(number);
         }
     } else {
-        console.log('Parking Lot is Empty');
+        console.log("Parking Lot is Empty")
     }
 }
 solve(['IN, CA2844AA',
