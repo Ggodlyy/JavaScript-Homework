e4ca6f8dce8eb060b711b3e38f76aa5c26bfc7f3
Ggodlyy <nikogerenski@abv.bg>
2021-06-02 16:56:54 +0300

    Update ShowMore.js

--- JavaScript-Advanced/DOM Introduction - Lab/04.Show More/ShowMore.js
+++ JavaScript-Advanced/DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -1,3 +1,9 @@
 function showText() {
-    // TODO
+    let hiddentText = document.querySelector('#text');
+    let link = document.querySelector('#more');
+
+    if (hiddentText.hasAttribute('style')) {
+        hiddentText.setAttribute('style', 'display:inline');
+        link.setAttribute('style', 'display:none');
+    }
 }
