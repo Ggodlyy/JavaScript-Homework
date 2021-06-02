dd0e68dec2704810171c28db8454103045974e3a
Ggodlyy <nikogerenski@abv.bg>
2021-06-02 16:35:19 +0300

    Update SumNumbers.js

--- JavaScript-Advanced/DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
+++ JavaScript-Advanced/DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,7 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    let num1 = document.querySelector('#num1');
+    let num2 = document.querySelector('#num2');
+    let sum = document.querySelector('#sum');
+
+    sum.value = Number(num1.value) + Number(num2.value);
 }
