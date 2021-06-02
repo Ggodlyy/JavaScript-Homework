1f22e02cb1dd7eceb5bd03134848327defeb4a62
Ggodlyy <nikogerenski@abv.bg>
2021-06-02 17:26:17 +0300

    Update ExtractParenthesis.js

--- JavaScript-Advanced/DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JavaScript-Advanced/DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,7 @@
 function extract(content) {
+    let el = document.getElementById(content);
+    let pattern = /\([\w ]+\)/g;
+    let result = el.textContent.match(pattern);
 
+    return result.join('; ');
 }
