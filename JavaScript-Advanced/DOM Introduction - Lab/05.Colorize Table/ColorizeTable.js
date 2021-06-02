5735f424cfb4426bcef98335f2630da549d40674
Ggodlyy <nikogerenski@abv.bg>
2021-06-02 17:08:45 +0300

    Update ColorizeTable.js

--- JavaScript-Advanced/DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
+++ JavaScript-Advanced/DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+    let tableRows = document.querySelectorAll('tr');
+    Array.from(tableRows).slice(1).forEach((tr, i)=> {
+        if (i % 2 === 0) {
+            tr.style.backgroundColor = 'Teal';  
+        }
+    });
 }
