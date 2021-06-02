43f8e7e9777d721add2f1f1e0611acabb893c802
Ggodlyy <nikogerenski@abv.bg>
2021-06-02 16:13:50 +0300

    Update EditElement.js

--- JavaScript-Advanced/DOM Introduction - Lab/01.Edit Element/EditElement.js
+++ JavaScript-Advanced/DOM Introduction - Lab/01.Edit Element/EditElement.js
@@ -1,3 +1,4 @@
-function editElement() {
-    // TODO
+function editElement(el, match, replacer) {
+    let pattern = RegExp(match, 'g');
+    el.textContent = el.textContent.replace(pattern, replacer);
 }
