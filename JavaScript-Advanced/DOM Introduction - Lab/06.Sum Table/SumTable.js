64efe3d63ffcc4319a5526e25190d6be0b9e07fb
Ggodlyy <nikogerenski@abv.bg>
2021-06-02 17:15:47 +0300

    Update SumTable.js

--- JavaScript-Advanced/DOM Introduction - Lab/06.Sum Table/SumTable.js
+++ JavaScript-Advanced/DOM Introduction - Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,13 @@
 function sumTable() {
+    let sum = document.querySelector('#sum');
+    let td = document.querySelectorAll('td');
+    let total = 0;
 
+    Array.from(td).forEach(td => {
+        if (!isNaN(Number(td.textContent))) {
+            total += Number(td.textContent);
+        }
+    });
+
+    sum.textContent = total;
 }
