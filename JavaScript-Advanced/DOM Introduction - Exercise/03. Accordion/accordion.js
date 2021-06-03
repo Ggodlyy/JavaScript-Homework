70fa003e2d27071c960fdbffd7ab00bfdc86ad70
Ggodlyy <nikogerenski@abv.bg>
2021-06-03 14:58:16 +0300

    Update accordion.js

--- JavaScript-Advanced/DOM Introduction - Exercise/03. Accordion/accordion.js
+++ JavaScript-Advanced/DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let hiddenText = document.querySelector('#extra');
+    let button = document.querySelector('.button');
+
+    if (hiddenText.style.display === 'none' || hiddenText.style.display === '') {
+        hiddenText.style.display = 'block';
+        button.textContent = 'Less';
+    } else {
+        hiddenText.style.display = 'none';
+        button.textContent = 'More';
+    }
 }
