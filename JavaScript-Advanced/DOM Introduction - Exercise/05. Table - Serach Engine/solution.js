909d527c9b34630b4ee5540306c82040d8c5d698
Ggodlyy <nikogerenski@abv.bg>
2021-06-03 15:35:19 +0300

    Update solution.js

--- JavaScript-Advanced/DOM Introduction - Exercise/05. Table - Serach Engine/solution.js
+++ JavaScript-Advanced/DOM Introduction - Exercise/05. Table - Serach Engine/solution.js
@@ -1,8 +1,18 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let searchField = document.querySelector('#searchField');
 
-   function onClick() {
-      //   TODO:
+   function onClick(e) {
+      let searchValue = searchField.value;
+      let trows = document.querySelector('tbody').children;
+      Array.from(trows).forEach(tr => {
+         Array.from(tr.children).forEach(td => {
+            if (td.textContent.includes(searchValue)) {
+               tr.setAttribute('class', 'select');
+            }
+         });
+      });
 
+      searchField.value = '';
    }
 }
