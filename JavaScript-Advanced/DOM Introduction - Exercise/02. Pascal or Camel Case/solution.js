ff0bb3fc95250cb497c60aae524fcadd2f760336
Ggodlyy <nikogerenski@abv.bg>
2021-06-03 14:47:45 +0300

    Update solution.js

--- JavaScript-Advanced/DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ JavaScript-Advanced/DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,39 @@
 function solve() {
-  //TODO...
+  let firstInput = document.querySelector('#text').value;
+  let secondInput = document.querySelector('#naming-convention').value;
+  let output = document.querySelector('#result');
+  let result = '';
+
+  if (secondInput === 'Camel Case') {
+    result = camelCaseConversion(firstInput);
+  } else if (secondInput === 'Pascal Case') {
+    result = pascalCaseConversion(firstInput);
+  } else {
+    result = 'Error!';
+  }
+
+  output.textContent = result;
+
+  function camelCaseConversion(text) {
+    let result = text.split(' ')
+      .map(string => string.toLowerCase())
+      .map((string, i) => {
+        if (i !== 0) {
+          return string[0].toUpperCase() + string.substring(1);
+        }
+        return string;
+      });
+
+    return result.join('');
+  }
+
+  function pascalCaseConversion(text) {
+    let result = text.split(' ')
+      .map(string => string.toLowerCase())
+      .map((string, i) => {
+        return string[0].toUpperCase() + string.substring(1);
+      });
+
+    return result.join('');
+  }
 }
