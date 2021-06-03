9393240d26a6a5efbbca57e3197101456fed2840
Ggodlyy <nikogerenski@abv.bg>
2021-06-03 16:49:45 +0300

    Update solution.js

--- JavaScript-Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
+++ JavaScript-Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+  const inputText = document.getElementById('input').value;
+  const outputElement = document.getElementById('output');
+
+  inputText
+    .match(/(?:\s*)([^.!?]+[.!?]+)/g)
+    .map(sentence => sentence.trim())
+    .reduce((acc, sentence, index) => {
+      if (index % 3 === 0) { acc.push([sentence]); } else { acc[acc.length - 1].push(sentence); }
+      return acc;
+    }, [])
+    .forEach(paragraph => {
+      let parHTML = document.createElement('p');
+      parHTML.textContent = paragraph;
+      outputElement.appendChild(parHTML);
+    });
 }
