99c53120585bab66f82457df8f2d77946102cb92
Ggodlyy <nikogerenski@abv.bg>
2021-06-03 15:13:19 +0300

    Update search.js

--- JavaScript-Advanced/DOM Introduction - Exercise/04. Search in List/search.js
+++ JavaScript-Advanced/DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,15 @@
 function search() {
-   // TODO
+   let list = document.querySelectorAll('li');
+   let searchValue = document.querySelector('#searchText').value;
+   let output = document.querySelector('#result');
+
+   let result = Array.from(list).filter(el => {
+      if (el.textContent.includes(searchValue)) {
+         el.style.textDecoration = 'underline';
+         el.style.fontWeight = 'bold';
+         return el;
+      }
+   });
+
+   output.textContent = `${result.length} matches found`;
 }
