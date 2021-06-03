13894aabe62b72dc26d6ada4473af54f17b17726
Ggodlyy <nikogerenski@abv.bg>
2021-06-03 14:16:50 +0300

    Update subtract.js

--- JavaScript-Advanced/DOM Introduction - Exercise/01. Subtraction/subtract.js
+++ JavaScript-Advanced/DOM Introduction - Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,8 @@
 function subtract() {
-    console.log('TODO:...');
+    let numberElements = document.querySelectorAll('input');
+    let output = document.querySelector('#result');
+
+    let firstNum = Number(numberElements[0].value);
+    let secondNum = Number(numberElements[1].value);
+    output.textContent = firstNum - secondNum;
 }
