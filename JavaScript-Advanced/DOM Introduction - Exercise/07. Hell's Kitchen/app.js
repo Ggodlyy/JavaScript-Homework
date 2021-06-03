7a6e916b49fb4450f5707a29839db42a93a88a37
Ggodlyy <nikogerenski@abv.bg>
2021-06-03 19:11:28 +0300

    Update app.js

--- JavaScript-Advanced/DOM Introduction - Exercise/07. Hell's Kitchen/app.js
+++ JavaScript-Advanced/DOM Introduction - Exercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,50 @@
 function solve() {
+   let bestRestaurantInfo = document.querySelector('#bestRestaurant p');
+   let bestWorkersInfo = document.querySelector('#workers p');
+
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
+   function onClick() {
+      let input = JSON.parse(document.querySelector('textarea').value);
+      let resaurantsInfo = {};
+
+      input.forEach(text => {
+         let [restaurantName, employeesInfo] = text.split(' - ');
+
+         if (!resaurantsInfo.hasOwnProperty(restaurantName)) {
+            resaurantsInfo[restaurantName] = [];
+         }
+
+         employeesInfo.split(', ').forEach(emplyeeInfo => {
+            let [employeeName, salary] = emplyeeInfo.split(' ');
+            let emplyeInfoObj = {
+               name: employeeName,
+               salary: Number(salary)
+            };
+            resaurantsInfo[restaurantName].push(emplyeInfoObj);
+         });
+      });
+
+      let bestAvgSalary = 0;
+      let bestRestaurant = '';
+
+      for (let restaurant in resaurantsInfo) {
+         let avgSalary = resaurantsInfo[restaurant].reduce((a, c) => a += c.salary, 0) / resaurantsInfo[restaurant].length;
+
+         if (avgSalary > bestAvgSalary) {
+            bestAvgSalary = avgSalary;
+            bestRestaurant = restaurant;
+         }
+      }
+
+      let result = resaurantsInfo[bestRestaurant].sort((a, b) => b.salary - a.salary);
+      let workerOuput = '';
+      result.forEach(obj => {
+         workerOuput += `Name: ${obj.name} With Salary: ${obj.salary} `;
+      });
+
+
+      bestRestaurantInfo.textContent = `Name: ${bestRestaurant} Average Salary: ${bestAvgSalary.toFixed(2)} Best Salary: ${(result[0].salary).toFixed(2)}`;
+      bestWorkersInfo.textContent = workerOuput.trim();
    }
 }
