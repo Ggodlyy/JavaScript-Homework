184e218718fdde7e78444de381bba20d6f63ed65
Ggodlyy <nikogerenski@abv.bg>
2021-06-04 15:58:05 +0300

    Update solution.js

--- JavaScript-Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
+++ JavaScript-Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,17 +1,15 @@
 function solve() {
-  const inputText = document.getElementById('input').value;
-  const outputElement = document.getElementById('output');
+  let inputText = document.getElementById('input').value;
+  let outputElement = document.getElementById('output');
 
-  inputText
-    .match(/(?:\s*)([^.!?]+[.!?]+)/g)
-    .map(sentence => sentence.trim())
-    .reduce((acc, sentence, index) => {
-      if (index % 3 === 0) { acc.push([sentence]); } else { acc[acc.length - 1].push(sentence); }
-      return acc;
-    }, [])
-    .forEach(paragraph => {
-      let parHTML = document.createElement('p');
-      parHTML.textContent = paragraph;
-      outputElement.appendChild(parHTML);
-    });
+  let sentences = inputText
+    .split('.')
+    .filter(el => el !== '')
+    .map(el => el.trim() + '.');
+
+  let maxP = Math.ceil(sentences.length / 3);
+
+  for (let i = 0; i < maxP; i++) {
+    outputElement.innerHTML += `<p>${sentences.splice(0, 3).join('')}</p>`
+  }
 }
