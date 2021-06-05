d4e4b0002b5ff1306448e5c127739fbdef3b7996
Ggodlyy <nikogerenski@abv.bg>
2021-06-05 21:06:33 +0300

    Update solution.js

--- JavaScript-Advanced/DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
+++ JavaScript-Advanced/DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,30 +1,68 @@
 function solve() {
-   let button = Array.from(document.getElementsByTagName('button'));
-   let textArea = document.getElementsByTagName('textarea')[0];
+   //with array
+   
+   let addButtons = document.querySelectorAll('.add-product');
+   let checkOutBtn = document.querySelector('.checkout');
+   let textAreaEl = document.querySelector('textarea');
+   let listOfProducts = [];
+   let total = 0;
 
-   let list = {};
+   Array.from(addButtons).forEach(btn => btn.addEventListener('click', addProduct));
+   checkOutBtn.addEventListener('click', calculateTotalMoney);
 
-   button.forEach(b => {
-      b.addEventListener("click", (e) => {
-         if (e.target.className === "add-product") {
-            let currentElement = e.target.parentElement;
+   function addProduct(e) {
+      let addProductDiv = e.target.parentElement;
+      let product = addProductDiv.previousElementSibling.firstElementChild.textContent;
+      let money = addProductDiv.nextElementSibling.textContent;
 
-            let priceElement = Number(currentElement.nextElementSibling.innerHTML);
-            let brandElement = currentElement.previousElementSibling.children[0].innerHTML;
+      if (!listOfProducts.includes(product)) {
+         listOfProducts.push(product);
+      }
 
-            if (!list[brandElement]) {
-               list[brandElement] = 0;
-            }
-            
-            list[brandElement] += priceElement;
+      let result = `Added ${product} for ${money} to the cart.\n`
+      textAreaEl.value += result;
+      total += Number(money);
+   }
 
-            textArea.value += `Added ${brandElement} for ${priceElement.toFixed(2)} to the cart.\n`
-         } else {
-            let totalPrice = Object.values(list).reduce((a, b) => a + b, 0);
-            textArea.value += `You bought ${Object.keys(list).join(', ')} for ${totalPrice.toFixed(2)}.`;
-
-            button.forEach(b => b.disabled = true)
-         }
-      })
-   })
+   function calculateTotalMoney(e) {
+      let allButtons = document.querySelectorAll('button');
+      textAreaEl.value += `You bought ${listOfProducts.join(', ')} for ${total.toFixed(2)}.`;
+      Array.from(allButtons).forEach(btn => btn.disabled = true);
+   }
 }
+
+//with Object
+
+// function solve() {
+//    let addButtons = document.querySelectorAll('.add-product');
+//    let checkOutBtn = document.querySelector('.checkout');
+//    let textAreaEl = document.querySelector('textarea');
+//    let listOfProducts = {};
+
+//    Array.from(addButtons).forEach(btn => btn.addEventListener('click', addProduct));
+//    checkOutBtn.addEventListener('click', calculateTotalMoney);
+
+//    function addProduct(e) {
+//       let addProductDiv = e.target.parentElement;
+//       let product = addProductDiv.previousElementSibling.firstElementChild.textContent;
+//       let money = addProductDiv.nextElementSibling.textContent;
+
+//       if (!listOfProducts[product]) {
+//          listOfProducts[product] = 0
+//       }
+
+//       listOfProducts[product] += Number(money);
+
+//       let result = `Added ${product} for ${money} to the cart.\n`
+//       textAreaEl.value += result;
+//    }
+
+
+
+//    function calculateTotalMoney(e) {
+//       let total = Object.values(listOfProducts).reduce((a, c) => a += c, 0);
+//       let allButtons = document.querySelectorAll('button');
+//       textAreaEl.value += `You bought ${Object.keys(listOfProducts).join(', ')} for ${total.toFixed(2)}.`;
+//       Array.from(allButtons).forEach(btn => btn.disabled = true);
+//    }
+// }
