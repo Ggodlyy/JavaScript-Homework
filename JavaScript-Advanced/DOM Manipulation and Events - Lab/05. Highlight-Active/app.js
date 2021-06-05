7be077a45f89777f76521a88945bb4e4f865f896
Ggodlyy <nikogerenski@abv.bg>
2021-06-05 18:54:59 +0300

    Update app.js

--- JavaScript-Advanced/DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ JavaScript-Advanced/DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,40 @@
 function focused() {
-    console.log('TODO:...');
+    let mainDiv = document.getElementsByTagName("div")[0];
+
+    Array.from(mainDiv.getElementsByTagName("input")).forEach(element => {
+        element.addEventListener("focus", focus);
+    });
+
+    Array.from(mainDiv.getElementsByTagName("input")).forEach(element => {
+        element.addEventListener("blur", focusLost);
+    });
+
+
+    function focus(e) {
+        let parent = e.target.parentNode;
+        parent.classList.add("focused");
+    }
+
+
+    function focusLost(e) {
+        let parent = e.target.parentNode;
+        parent.classList.remove("focused");
+    }
+
 }
+
+// function betterSolution() {
+//     let firstDiv = document.querySelector('body > div');
+//     firstDiv.addEventListener('focusin', changeBackground);
+//     firstDiv.addEventListener('focusout', blurFunc);
+
+//     function changeBackground(e) {
+//         let divEl = e.target.parentElement;
+//         divEl.classList.add('focused')
+//     }
+
+//     function blurFunc(e) {
+//         let divEl = e.target.parentElement;
+//         divEl.classList.remove('focused');
+//     }
+// }
