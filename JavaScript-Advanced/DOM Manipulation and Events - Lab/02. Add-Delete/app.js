0a3e843956f951eb81473bb412d36968e5288c68
Ggodlyy <nikogerenski@abv.bg>
2021-06-05 17:38:28 +0300

    Update app.js

--- JavaScript-Advanced/DOM Manipulation and Events - Lab/02. Add-Delete/app.js
+++ JavaScript-Advanced/DOM Manipulation and Events - Lab/02. Add-Delete/app.js
@@ -1,3 +1,20 @@
 function addItem() {
-    //TODO...
+    let ulEL = document.querySelector('#items');
+    let inputEl = document.querySelector('#newItemText');
+
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    a.textContent = '[Delete]';
+    a.href = '#';
+    a.addEventListener('click', remove);
+    li.textContent = inputEl.value;
+
+    function remove(e) {
+        let li = e.target.parentElement;
+        li.remove();
+    }
+
+    li.appendChild(a);
+    ulEL.appendChild(li);
+    inputEl.value = '';
 }
