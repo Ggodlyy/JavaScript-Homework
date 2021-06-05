b084659d385142bb2aca40222c9e10d83906ab32
Ggodlyy <nikogerenski@abv.bg>
2021-06-05 20:36:50 +0300

    Update solution.js

--- JavaScript-Advanced/DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
+++ JavaScript-Advanced/DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,30 +1,29 @@
 function solve() {
-   let button = document.getElementsByTagName("button")
-   let textArea = document.getElementsByTagName("textarea")[0]
+   let button = Array.from(document.getElementsByTagName('button'));
+   let textArea = document.getElementsByTagName('textarea')[0];
 
-   let list = {}
+   let list = {};
 
-   Array.from(button).forEach(b => {
+   button.forEach(b => {
       b.addEventListener("click", (e) => {
          if (e.target.className === "add-product") {
-            let currentElement = e.target.parentElement
+            let currentElement = e.target.parentElement;
 
-            let priceElement = Number(currentElement.nextElementSibling.innerHTML)
-            let brandElement = currentElement.previousElementSibling.children[0].innerHTML
+            let priceElement = Number(currentElement.nextElementSibling.innerHTML);
+            let brandElement = currentElement.previousElementSibling.children[0].innerHTML;
 
             if (!list[brandElement]) {
-               list[brandElement] = 0
+               list[brandElement] = 0;
             }
-            list[brandElement] += priceElement
+            
+            list[brandElement] += priceElement;
 
-            textArea.innerHTML += `Added ${brandElement} for ${priceElement.toFixed(2)} to the cart.\n`
+            textArea.value += `Added ${brandElement} for ${priceElement.toFixed(2)} to the cart.\n`
          } else {
+            let totalPrice = Object.values(list).reduce((a, b) => a + b, 0);
+            textArea.value += `You bought ${Object.keys(list).join(', ')} for ${totalPrice.toFixed(2)}.`;
 
-            let totalPrice = Number(Object.values(list).reduce((a, b) => a + b, 0))
-            textArea.innerHTML += `You bought ${Object.keys(list).join(', ')} for ${totalPrice.toFixed(2)}.`
-
-            Array.from(button).forEach(b => b.disabled = true)
-
+            button.forEach(b => b.disabled = true)
          }
       })
    })
