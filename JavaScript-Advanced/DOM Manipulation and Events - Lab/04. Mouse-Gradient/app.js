d56b191730e695c34713dd5e744451a717bc935c
Ggodlyy <nikogerenski@abv.bg>
2021-06-05 18:27:22 +0300

    Update app.js

--- JavaScript-Advanced/DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
+++ JavaScript-Advanced/DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,17 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientEL = document.querySelector('#gradient');
+    let output = document.querySelector('#result');
+
+    gradientEL.addEventListener('mousemove', gradientMove);
+    gradientEL.addEventListener('mouseout', gradientOut);
+
+    function gradientMove(e) {
+        let power = e.offsetX / (e.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+        output.textContent = power + "%";
+    }
+
+    function gradientOut(e) {
+        output.textContent = '';
+    }
 }
