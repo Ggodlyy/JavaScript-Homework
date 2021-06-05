bf55524502794dac43bc47eb28d9bff247e4283e
Ggodlyy <nikogerenski@abv.bg>
2021-06-05 19:24:51 +0300

    Update app.js

--- JavaScript-Advanced/DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ JavaScript-Advanced/DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    console.log('TODO:...');
+    let pattern = /[a-z]+@[a-z]+\.[a-z]+/;
+    let inputEl = document.querySelector('#email');
+
+    inputEl.addEventListener('change', validation);
+
+    function validation(e) {
+        let match = inputEl.value.match(pattern);
+
+        if (match) {
+            inputEl.classList.remove('error');
+        } else {
+            inputEl.classList.add('error');
+        }
+    }
 }
