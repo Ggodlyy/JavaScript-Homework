83a2d1378850ecc2ef0c91b097995c26bc6504f6
Ggodlyy <nikogerenski@abv.bg>
2021-06-05 18:13:13 +0300

    Update app.js

--- JavaScript-Advanced/DOM Manipulation and Events - Lab/03. Delete-From-Table/app.js
+++ JavaScript-Advanced/DOM Manipulation and Events - Lab/03. Delete-From-Table/app.js
@@ -1,3 +1,15 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let inputEl = document.querySelector('input[name="email"]');
+    let allTd = document.querySelectorAll('tbody tr td:last-child');
+    let output = document.querySelector('#result');
+
+    let result = Array.from(allTd).find(td => td.textContent === inputEl.value);
+
+    if (result) {
+        let trEl = result.parentElement;
+        trEl.remove();
+        output.textContent = 'Deleted.';
+    } else {
+        output.textContent = 'Not found.';
+    }
 }
