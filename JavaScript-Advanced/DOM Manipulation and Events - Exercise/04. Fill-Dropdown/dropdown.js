26bc9f87652986a90f9f3fdb2564a5c50991e684
Ggodlyy <nikogerenski@abv.bg>
2021-06-07 17:25:14 +0300

    Update dropdown.js

--- JavaScript-Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ JavaScript-Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    let inputEl = document.querySelector('#newItemText');
+    let valueEl = document.querySelector('#newItemValue');
+    let menu = document.querySelector('#menu');
+
+    let option = document.createElement('option');
+    option.textContent = inputEl.value;
+    option.value = valueEl.value;
+    menu.appendChild(option);
+
+    inputEl.value = '';
+    valueEl.value = '';
 }
