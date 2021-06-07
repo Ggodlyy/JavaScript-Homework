48549951b54a76861b97182d6cba06ec9123fec5
Ggodlyy <nikogerenski@abv.bg>
2021-06-07 17:16:31 +0300

    Update app.js

--- JavaScript-Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ JavaScript-Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = document.querySelectorAll('button');
+
+    Array.from(buttons).forEach(btn => btn.addEventListener('click', showInfo));
+
+    function showInfo(e) {
+        let profile = e.target.parentElement;
+        let lockedInput = profile.querySelector('input[value="lock"]');
+
+        if (!lockedInput.checked) {
+            let hiddenField = profile.querySelector('div');
+
+            if (e.target.textContent === 'Hide it') {
+                hiddenField.style.display = 'none';
+                e.target.textContent = 'Show more';
+            } else {
+                hiddenField.style.display = 'block';
+                e.target.textContent = 'Hide it';
+            }
+        }
+    }
 }
