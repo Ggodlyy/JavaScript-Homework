cd1ef3171587df397e3d405a10e6965e86799216
Ggodlyy <nikogerenski@abv.bg>
2021-06-07 15:31:02 +0300

    Update script.js

--- JavaScript-Advanced/DOM Manipulation and Events - Exercise/01. Sections/script.js
+++ JavaScript-Advanced/DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+   let output = document.querySelector('#content');
+   words.forEach(w => {
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      p.textContent = w;
+      p.style.display = 'none';
+
+      div.addEventListener('click', (e) => {
+         let p = e.currentTarget.firstElementChild;
+         p.style.display = 'block';
+      });
+
+      div.appendChild(p);
+      output.appendChild(div);
+   });
 }
