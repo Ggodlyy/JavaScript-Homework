81732917b7465a7afbe0be67c5c7a2e810120bd1
Ggodlyy <nikogerenski@abv.bg>
2021-06-07 16:49:52 +0300

    Update solution.js

--- JavaScript-Advanced/DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
+++ JavaScript-Advanced/DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,30 @@
 function attachEventsListeners() {
+    let buttons = document.querySelectorAll('input[type="button"]');
 
-    console.log('TODO:...');
+    Array.from(buttons).forEach(btn => {
+        btn.addEventListener('click', convertTime);
+    });
+
+    function convertTime(e) {
+        let input = e.target.previousElementSibling;
+        let inputName = input.previousElementSibling.textContent;
+        inputName = inputName.substring(0, inputName.length - 2);
+        let days = document.querySelector('#days');
+        let hours = document.querySelector('#hours');
+        let minutes = document.querySelector('#minutes');
+        let seconds = document.querySelector('#seconds');
+        let value = Number(input.value);
+        let daysConversion = {
+            'Days': () => value,
+            'Hours': () => value / 24,
+            'Minutes': () => value / 1440,
+            'Seconds': () => value / 86400
+        };
+
+        let daysValue = daysConversion[inputName]();
+        days.value = daysValue;
+        hours.value = daysValue * 24;
+        minutes.value = daysValue * 1440;
+        seconds.value = daysValue * 86400;
+    }
 }
