25a590c95ad5e3c31305db86bdd4208f53e0f32b
Ggodlyy <nikogerenski@abv.bg>
2021-06-07 17:56:10 +0300

    Update app.js

--- JavaScript-Advanced/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ JavaScript-Advanced/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,31 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let inputArea = document.querySelector('textarea[placeholder="Write your message here..."]');
+    let outputArea = document.querySelector('textarea[placeholder="No messages..."]');
+    let [encodeBtn, decodeBtn] = document.querySelectorAll('button');
+
+    encodeBtn.addEventListener('click', encodeMessage);
+    decodeBtn.addEventListener('click', decodeMessage);
+
+    function encodeMessage(e) {
+        let message = inputArea.value;
+        let result = message
+            .split('')
+            .map(char => char.charCodeAt(0) + 1)
+            .map(num => String.fromCharCode(num))
+            .join('');
+
+        outputArea.value = result;
+        inputArea.value = '';
+    }
+
+    function decodeMessage(e) {
+        let message = outputArea.value;
+        let result = message
+            .split('')
+            .map(char => char.charCodeAt(0) - 1)
+            .map(num => String.fromCharCode(num))
+            .join('');
+
+        outputArea.value = result;
+    }
 }
