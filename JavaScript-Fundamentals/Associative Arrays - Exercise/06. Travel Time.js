f9412bd80dc25871be16e3c5835b505bcb9b37bc
Ggodlyy <nikogerenski@abv.bg>
2021-06-08 19:16:16 +0300

    Update 06. Travel Time.js

--- JavaScript-Fundamentals/Associative Arrays - Exercise/06. Travel Time.js
+++ JavaScript-Fundamentals/Associative Arrays - Exercise/06. Travel Time.js
@@ -3,48 +3,36 @@ function solve(arr) {
 
     for (let input of arr) {
         let [country, town, cost] = input.split(' > ');
-        let townIfo = {
-            [town]: Number(cost)
-        };
+        cost = Number(cost);
 
-        if (obj.hasOwnProperty(country)) {
-            let index = obj[country].find(obj => obj.hasOwnProperty(town));
-
-            if (index) {
-                obj[country].forEach(innerObj => {
-                    if (innerObj.hasOwnProperty(town)) {
-                        innerObj[town] > Number(cost) ? innerObj[town] = Number(cost) : innerObj[town];
-                    }
-                });
-                continue;
-            }
-        } else {
-            obj[country] = [];
+        if (!obj.hasOwnProperty(country)) {
+            obj[country] = {};
         }
 
-        obj[country].push(townIfo);
+        if (!obj[country].hasOwnProperty(town)) {
+            obj[country][town] = cost;
+        }
+
+        if (obj[country][town] > cost) {
+            obj[country][town] = cost;
+        }
     }
 
+    let ordered = Object.entries(obj).sort((a, b) => a[0].localeCompare(b[0]));
 
-    let result = Object.entries(obj).sort((a, b) => a[0].localeCompare(b[0]));
-
-    for (let arr of result) {
-        let townInfoText = [];
-        arr[1].sort((a, b) => Object.values(a) - Object.values(b));
-
-        arr[1].forEach(innerObj => {
-            for (let key in innerObj) {
-                townInfoText.push(`${key} -> ${innerObj[key]}`);
-            }
+    for (let matrix of ordered) {
+        let result = [];
+        Object.values(matrix[1]).sort((a, b) => a - b);
+        Object.entries(matrix[1]).forEach(kvp => {
+            result.push(`${kvp[0]} -> ${kvp[1]}`);
         });
-
-        console.log(`${arr[0]} -> ${townInfoText.join(' ')}`);
+        console.log(`${matrix[0]} -> ${result.join(' ')}`);
     }
 }
 solve([
     "Bulgaria > Sofia > 500",
     "Bulgaria > Sopot > 800",
-    "Bulgaria > Sopot > 900",
+    "Bulgaria > Sopot > 200",
     "France > Paris > 2000",
     "Albania > Tirana > 1000",
 ]
