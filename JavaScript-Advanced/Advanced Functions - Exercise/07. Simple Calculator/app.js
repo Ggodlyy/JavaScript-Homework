4af86f65b54d9ed9d9ffcac8e5659df03ec04e9f
Ggodlyy <nikogerenski@abv.bg>
2021-06-10 18:22:30 +0300

    Update app.js

--- JavaScript-Advanced/Advanced Functions - Exercise/07. Simple Calculator/app.js
+++ JavaScript-Advanced/Advanced Functions - Exercise/07. Simple Calculator/app.js
@@ -1,6 +1,29 @@
 function calculator() {
-    // TODO:
+    function init(select1, selector2, resultSelector) {
+        this.firstNum = document.querySelector(select1);
+        this.secondNum = document.querySelector(selector2);
+        this.result = document.querySelector(resultSelector);
+    }
+
+    function add() {
+        this.result.value = Number(this.firstNum.value) + Number(this.secondNum.value);
+    }
+
+    function subtract() {
+        this.result.value = Number(this.firstNum.value) - Number(this.secondNum.value);
+    }
+
+    return {
+        init,
+        add,
+        subtract
+    }
 }
+
+const calculate = calculator();
+calculate.init('#num1', '#num2', '#result');
+
+
 
 
 
