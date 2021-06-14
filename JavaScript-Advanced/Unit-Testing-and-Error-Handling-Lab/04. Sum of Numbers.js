f1e3d1cb7498f8dad481d8db1c350a4e69218252
Ggodlyy <nikogerenski@abv.bg>
2021-06-14 16:16:17 +0300

    Update

--- JavaScript-Advanced/Unit-Testing-and-Error-Handling-Lab/04. Sum of Numbers.js
+++ JavaScript-Advanced/Unit-Testing-and-Error-Handling-Lab/04. Sum of Numbers.js
@@ -6,4 +6,4 @@ function sum(arr) {
     return sum;
 }
 
-module.exports = sum();
+module.exports = sum;
--- JavaScript-Advanced/Unit-Testing-and-Error-Handling-Lab/test/sum.test.js
+++ JavaScript-Advanced/Unit-Testing-and-Error-Handling-Lab/test/sum.test.js
@@ -1,12 +1,12 @@
-let sumFunc = require('../04. Sum of Numbers');
+let sum = require('../04. Sum of Numbers');
 let assert = require('chai').assert;
 
 describe('Should take Array of numbers and return their sum', () => {
     it ('should add numbers', () => {
-        let numbers = [1, 2, 3, 4];
+        let input = [1, 2, 3, 4];
         let expectedResult = 10;
-        let actualResult = sumFunc(numbers);
+        let actualResult = sum(input);
 
-        assert.equal(expectedResult === actualResult);
+        assert.equal(actualResult, expectedResult);
     });
 });
