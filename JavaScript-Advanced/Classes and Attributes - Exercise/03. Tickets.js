2a40238735a4d7e7ee999221bad6cbf3a9ed5f32
Ggodlyy <nikogerenski@abv.bg>
2021-06-17 14:34:29 +0300

    Create 03. Tickets.js

---
+++ JavaScript-Advanced/Classes and Attributes - Exercise/03. Tickets.js
@@ -0,0 +1,30 @@
+function solve(arr, critetria) {
+    class Ticket {
+        constructor(destination, price, status) {
+            this.destination = destination;
+            this.price = Number(price);
+            this.status = status;
+        }
+    }
+
+    let tickets = [];
+
+    arr.forEach(input => {
+        let [destination, price, status] = input.split('|');
+        tickets.push(new Ticket(destination, price, status));
+    });
+
+    if (critetria === 'price') {
+        tickets.sort((a, b) => a[critetria] - b[critetria]);
+    } else {
+        tickets.sort((a, b) => a[critetria].localeCompare(b[critetria]));
+    }
+
+    return tickets;
+}
+solve(['Philadelphia|94.20|available',
+    'New York City|95.99|available',
+    'New York City|95.99|sold',
+    'Boston|126.20|departed'],
+    'destination'
+)
