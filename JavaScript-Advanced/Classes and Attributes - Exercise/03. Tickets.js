e72056d614168f9d1df0127ed044dc62fe52f697
Ggodlyy <nikogerenski@abv.bg>
2021-06-17 14:41:04 +0300

    Update 03. Tickets.js

--- JavaScript-Advanced/Classes and Attributes - Exercise/03. Tickets.js
+++ JavaScript-Advanced/Classes and Attributes - Exercise/03. Tickets.js
@@ -20,7 +20,6 @@ function solve(arr, critetria) {
         tickets.sort((a, b) => a[critetria].localeCompare(b[critetria]));
     }
 
-
     // Another way with a compare function
 
     //tickets.sort(sortThem);
