f8220670fb42dbcea7cc8d0c15452cc2e4c06183
Ggodlyy <nikogerenski@abv.bg>
2021-06-18 16:41:29 +0300

    Update formValidation.js

--- JavaScript-Advanced/Unit-Testing-and-Error-Handling-Exercise/07.Form-Validation/formValidation.js
+++ JavaScript-Advanced/Unit-Testing-and-Error-Handling-Exercise/07.Form-Validation/formValidation.js
@@ -1,127 +1,71 @@
 function validate() {
-    let usernameInput = document.querySelector('#username');
-    let emailInput = document.querySelector('#email');
-    let passwordInput = document.querySelector('#password');
-    let confirmPasswordInput = document.querySelector('#confirm-password');
     let companyInput = document.querySelector('#company');
-    let companyNumberInput = document.querySelector('#companyNumber');
     let submitBtn = document.querySelector('#submit');
 
 
-    submitBtn.addEventListener('click', validateInputs);
+    submitBtn.addEventListener('click', validateForm);
     companyInput.addEventListener('change', isCompanyCheck);
 
-    function validateInputs(e) {
+    function validateForm(e) {
         e.preventDefault();
-        usernameValidator();
-        emailValidator();
-        passwordValidator();
-        confirmPasswordValidator();
-        companyNumberValidator();
-        validFields();
-    }
 
-    function usernameValidator() {
-        let username = usernameInput.value;
-        let pattern = /^[A-Za-z0-9]{3,20}$/;
+        let usernameInput = document.querySelector('#username');
+        let usernameRegexPattern = /^[A-Za-z0-9]{3,20}$/;
+        let validUsername = usernameRegexPattern.test(usernameInput.value);
+        setBorder(usernameInput, validUsername);
+        let emailInput = document.querySelector('#email');
+        let emailRegexPattern = /^.*@.*\..*$/;
+        let validEmail = emailRegexPattern.test(emailInput.value);
+        setBorder(emailInput, validEmail);
+        let passwordInput = document.querySelector('#password');
+        let passwordRegexPattern = /^\w{5,15}$/;
+        let validPassword = passwordRegexPattern.test(passwordInput.value);
+        let confirmPasswordInput = document.querySelector('#confirm-password');
+        let bothPasswordsAreOk = validPassword && passwordInput.value === confirmPasswordInput.value;
+        setBorder(passwordInput, bothPasswordsAreOk);
+        setBorder(confirmPasswordInput, bothPasswordsAreOk);
 
-        if (pattern.test(username)) {
-            if (usernameInput.hasAttribute('style')) {
-                usernameInput.setAttribute('style', 'border:none');
+        let companyNumberInput = document.querySelector('#companyNumber');
+        let validCompanyNumber = false;
+        if (companyNumberInput.value.trim() !== '' && !isNaN(Number(companyNumberInput.value))) {
+            let companyNumber = Number(companyNumberInput.value);
+            if (companyNumber >= 1000 && companyNumber <= 9999) {
+                validCompanyNumber = true;
             }
+        }
+      
+        setBorder(companyNumberInput, validCompanyNumber);
+
+        let validDiv = document.querySelector('#valid');
+        let mainValidInputs = validUsername && validEmail && validPassword && bothPasswordsAreOk;
+
+        if (mainValidInputs) {
+            validDiv.style.display = 'block';
+            if (companyInput.checked) {
+               if (!validCompanyNumber) {
+                validDiv.style.display = 'none';
+               }
+            } 
         } else {
-            usernameInput.setAttribute('style', 'border-color:red');
+            validDiv.style.display = 'none';
         }
     }
 
-    function emailValidator() {
-        let email = emailInput.value;
-        let pattern = /^[A-Za-z0-9_]+@[A-Za-z0-9_]+\.[A-Za-z0-9_]+$/;
-
-        if (pattern.test(email)) {
-            if (emailInput.hasAttribute('style')) {
-                emailInput.setAttribute('style', 'border:none');
-            }
-        } else {
-            emailInput.setAttribute('style', 'border-color:red');
-        }
-    }
-
-    function passwordValidator() {
-        let password = passwordInput.value;
-        let pattern = /^[A-Za-z0-9_]{5,15}$/;
-
-        if (pattern.test(password)) {
-            if (passwordInput.hasAttribute('style')) {
-                passwordInput.setAttribute('style', 'border:none');
-            }
-        } else {
-            passwordInput.setAttribute('style', 'border-color:red');
-        }
-    }
-
-    function confirmPasswordValidator() {
-        let confirmPassword = confirmPasswordInput.value;
-        let originalPassword = document.querySelector('#password').value;
-        let pattern = /^[A-Za-z0-9_]{5,15}$/;
-
-        if (confirmPassword === originalPassword && pattern.test(confirmPassword)) {
-            if (confirmPasswordInput.hasAttribute('style')) {
-                confirmPasswordInput.setAttribute('style', 'border:none');
-            }
-        } else {
-            confirmPasswordInput.setAttribute('style', 'border-color:red');
-        }
-    }
-
-    function isCompanyCheck() {
+    function isCompanyCheck(e) {
         let companyInfo = document.querySelector('#companyInfo');
 
-        if (companyInput.checked === true) {
+        if (e.target.checked === true) {
             companyInfo.style.display = 'block';
         } else {
             companyInfo.style.display = 'none';
         }
     }
 
-    function companyNumberValidator() {
-        let companyNumber = companyNumberInput.value;
-
-        if (companyInput.checked === true) {
-            if (companyNumber >= 1000 && companyNumber <= 9999) {
-                if (companyNumberInput.hasAttribute('style')) {
-                    companyNumberInput.setAttribute('style', 'border:none');
-                }
-            } else {
-                companyNumberInput.setAttribute('style', 'border-color:red');
-            }
-        }
-    }
-
-    function validFields() {
-        let validDiv = document.querySelector('#valid');
-
-        if (companyInput.checked !== true) {
-            if (usernameInput.style.border === 'none' &&
-                emailInput.style.border === 'none' &&
-                passwordInput.style.border === 'none' &&
-                confirmPasswordInput.style.border === 'none') {
-                validDiv.style.display = 'block';
-                console.log('all are correct');
-            } else {
-                validDiv.style.display = 'none';
-            }
+    function setBorder(input, isValid) {
+        if (isValid) {
+            input.style.setProperty('border', 'none');
         } else {
-            if (usernameInput.style.border === 'none' &&
-            emailInput.style.border === 'none' &&
-            passwordInput.style.border === 'none' &&
-            confirmPasswordInput.style.border === 'none' &&
-            companyNumberInput.style.border === 'none') {
-            validDiv.style.display = 'block';
-            console.log('all are correct');
-        } else {
-            validDiv.style.display = 'none';
-        }
+            input.style.setProperty('border', '2px solid red');
         }
     }
 }
