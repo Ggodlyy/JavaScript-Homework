5ad5bf3461993d413a0b7202f992060c5b5f92e5
Ggodlyy <nikogerenski@abv.bg>
2021-06-19 20:28:55 +0300

    Create 01. Array Extension.js

---
+++ JavaScript-Advanced/Prototypes and Inheritance - Exercise/01. Array Extension.js
@@ -0,0 +1,25 @@
+(function solve() {
+    Array.prototype.last = function () {
+        return this[this.length - 1];
+    };
+
+    Array.prototype.skip = function (n) {
+        return this.slice(n);
+    };
+
+    Array.prototype.take = function (n) {
+        return this.slice(0, n);
+    }
+
+    Array.prototype.sum = function () {
+        return this.reduce((a, c) => a += c, 0);
+    }
+
+    Array.prototype.average = function() {
+        return this.sum() / this.length;
+    }
+})();
+
+let arr = [0, 1, 2, 3];
+
+console.log(arr.take(2));
