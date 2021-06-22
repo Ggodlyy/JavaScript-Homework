c977e632f1aaa3afca4e1d77a26c7ffbc38d394f
Ggodlyy <nikogerenski@abv.bg>
2021-06-22 15:29:58 +0300

    Update 02. String Extension.js

--- JavaScript-Advanced/Prototypes and Inheritance - Exercise/02. String Extension.js
+++ JavaScript-Advanced/Prototypes and Inheritance - Exercise/02. String Extension.js
@@ -1,55 +1,78 @@
 (function solve() {
     String.prototype.ensureStart = function (str) {
-        if (!this.includes(str)) {
-            return str + this;
-        } else {
-            return this;
+        if (!this.startsWith(str)) {
+            return str + this.toString();
         }
+
+        return this.toString();
     };
 
     String.prototype.ensureEnd = function (str) {
-        if (!this.includes(str)) {
-            return this + str;
-        } else {
-            return this;
+        if (!this.endsWith(str)) {
+            return this.toString() + str;
         }
+
+        return this.toString();
     };
 
     String.prototype.isEmpty = function () {
-        this === '' ? true : false;
+        return this.toString() === '' ? true : false;
     };
 
     String.prototype.truncate = function (n) {
-        if (this.length < 4) {
-            return '.'.repeat(n);
-        } else if (this.length < n) {
-            return this;
-        } else {
-            if (this.includes(' ')) {
-                let arr = this.split(' ');
-             
-            } else {
-
-            }
+        if (this.length <= n) {
+            return this.toString();
         }
+
+        if (this.includes(' ')) {
+            let arr = this.split(' ');
+
+            while (arr.join(' ').length + 3 > n) {
+                arr.pop();
+            }
+
+            return arr.join(' ') + '...';
+        }
+
+        if (n < 4) {
+            return '.'.repeat(n);
+        }
+
+        return this.substring(0, n - 3) + '...';
     };
 
-    String.myTest = function() {
-        console.log(this);
+    String.format = function (string, ...params) {
+        if (params !== undefined) {
+            params.forEach(w => string = string.replace(/{\d+}/, w));
+
+            return string;
+        }
     }
 })();
 
 let str = 'my string';
 str = str.ensureStart('my');
-String.myTest();
-// str = str.ensureStart('hello ');
-// str = str.truncate(16);
-// str = str.truncate(14);
-// str = str.truncate(8);
-// str = str.truncate(4);
-// str = str.truncate(2);
-// str = String.format('The {0} {1} fox',
-//     'quick', 'brown');
-// str = String.format('jumps {0} {1}',
-//     'dog');
+console.log(str);
+str = str.ensureStart('hello ');
+console.log(str);
+console.log(str.isEmpty());
+str = str.truncate(16);
+console.log(str);
+str = str.truncate(14);
+console.log(str);
+str = str.truncate(8);
+console.log(str);
+str = str.truncate(4);
+console.log(str);
+str = str.truncate(2);
+console.log(str);
+str = String.format('The {0} {1} fox',
+    'quick', 'brown');
+
+console.log(str);
+
+str = String.format('jumps {0} {1}',
+    'dog');
+
+console.log(str);
 
