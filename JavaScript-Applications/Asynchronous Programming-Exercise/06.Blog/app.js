744d008681f92394a563b425fc42678c9266ecd7
Ggodlyy <nikogerenski@abv.bg>
2021-07-08 19:34:18 +0300

    Update

--- JavaScript-Applications/Asynchronous Programming-Exercise/06.Blog/app.js
+++ JavaScript-Applications/Asynchronous Programming-Exercise/06.Blog/app.js
@@ -1,5 +1,52 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.querySelector('#btnLoadPosts').addEventListener('click', loadPosts);
+    document.querySelector('#btnViewPost').addEventListener('click', viewPost);
+
+    function loadPosts(e) {
+        fetch('http://localhost:3030/jsonstore/blog/posts')
+            .then(res => res.json())
+            .then(data => {
+                Object.keys(data).forEach(key => {
+                    let postOptionEl = document.createElement('option');
+                    postOptionEl.value = key;
+                    postOptionEl.textContent = data[key].title;
+                    document.querySelector('#posts').appendChild(postOptionEl);
+                });
+            });
+    }
+
+    function viewPost(e) {
+        let input = document.querySelector('#posts');
+        let postUrl = 'http://localhost:3030/jsonstore/blog/posts/' + input.value;
+        let commentsUrl = 'http://localhost:3030/jsonstore/blog/comments/';
+        console.log(postUrl);
+        console.log(commentsUrl + input.value);
+
+        Promise.all(
+            [
+                fetch(postUrl).then(res => res.json()),
+                fetch(commentsUrl).then(res => res.json())
+            ]
+        ).then(([postObj, comentsObj]) => {
+            console.log(postObj);
+            console.log(comentsObj);
+            let postTitle = document.querySelector('#post-title');
+            let postBody = document.querySelector('#post-body');
+
+            postTitle.textContent = postObj.title;
+            postBody.textContent = postObj.body;
+
+            Object.values(comentsObj).forEach(cObj => {
+                if (cObj.postId === postObj.id) {
+                    let comment = document.createElement('li');
+                    comment.textContent = cObj.text;
+                    comment.id = cObj.postId;
+                    document.querySelector('#post-comments').appendChild(comment);
+                }
+            });
+        })
+
+    }
 }
 
 attachEvents();
