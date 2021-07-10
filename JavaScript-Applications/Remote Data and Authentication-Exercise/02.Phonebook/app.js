46d79626560ed265ae50be08a15bad80921f2645
Ggodlyy <nikogerenski@abv.bg>
2021-07-10 16:53:11 +0300

    Update app.js

--- JavaScript-Applications/Remote Data and Authentication-Exercise/02.Phonebook/app.js
+++ JavaScript-Applications/Remote Data and Authentication-Exercise/02.Phonebook/app.js
@@ -1,2 +1,10 @@
+document.querySelector('#btnLoad').addEventListener('click', loadPhonebook);
 
+function loadPhonebook(e) {
+    fetch('http://localhost:3030/jsonstore/phonebook')
+        .then(res => res.json())
+        .then(data => {
+            Object.values(data).forEach(obj => console.log(obj));
+        });
+}
 
