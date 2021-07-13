dc7b192b06559baaf23f59608a35df0468232372
Ggodlyy <nikogerenski@abv.bg>
2021-07-13 21:12:49 +0300

    Update app.js

--- JavaScript-Applications/Remote Data and Authentication-Exercise/02.Phonebook/app.js
+++ JavaScript-Applications/Remote Data and Authentication-Exercise/02.Phonebook/app.js
@@ -1,11 +1,11 @@
 document.querySelector('#btnLoad').addEventListener('click', loadPhonebook);
-document.querySelector('#btnCreate').addEventListener('click', createPhoneInfo);
+document.querySelector('#btnCreate').addEventListener('click', createPhonebook);
+let output = document.querySelector('#phonebook');
 
 function loadPhonebook(e) {
     fetch('http://localhost:3030/jsonstore/phonebook')
         .then(res => res.json())
         .then(data => {
-            let output = document.querySelector('#phonebook');
             Array.from(output.children).forEach(tr => tr.remove());
 
             Object.values(data).forEach(obj => {
@@ -14,47 +14,9 @@ function loadPhonebook(e) {
             });
         })
         .catch(err => console.log(err));
-
-    function createPhoneInfo(name, phone, id) {
-        let trEl = document.createElement('tr');
-
-        let nameTd = document.createElement('td');
-        nameTd.textContent = name;
-
-        let phoneTd = document.createElement('td');
-        phoneTd.textContent = phone;
-
-        let btnTdWrapper = document.createElement('td');
-
-        let deleteBtn = document.createElement('button');
-        deleteBtn.setAttribute('data-id', id);
-        deleteBtn.textContent = 'Delete';
-        deleteBtn.addEventListener('click', deletePersonInfo);
-
-        function deletePersonInfo(e) {
-            let id = e.target.getAttribute('data-id');
-
-            fetch('http://localhost:3030/jsonstore/phonebook/' + id, {
-                method: 'DELETE'
-            })
-                .then(res => res.json())
-                .then(data => console.log(data))
-                .catch(err => console.log(err));
-
-            let trElement = e.target.parentElement.parentElement;
-            trElement.remove();
-        }
-
-        btnTdWrapper.appendChild(deleteBtn);
-        trEl.appendChild(nameTd);
-        trEl.appendChild(phoneTd);
-        trEl.appendChild(btnTdWrapper);
-
-        return trEl;
-    }
 }
 
-function createPhoneInfo(e) {
+function createPhonebook(e) {
     let personInput = document.querySelector('#person');
     let phoneInput = document.querySelector('#phone');
 
@@ -69,12 +31,52 @@ function createPhoneInfo(e) {
         })
     })
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            let phoneInfo = createPhoneInfo(data.person, data.phone, data._id);
+            output.appendChild(phoneInfo);
+        })
         .catch(err => console.log(err));
 
     personInput.value = '';
     phoneInput.value = '';
-    loadPhonebook();
+}
+
+function createPhoneInfo(name, phone, id) {
+    let trEl = document.createElement('tr');
+
+    let nameTd = document.createElement('td');
+    nameTd.textContent = name;
+
+    let phoneTd = document.createElement('td');
+    phoneTd.textContent = phone;
+
+    let btnTdWrapper = document.createElement('td');
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('data-id', id);
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.addEventListener('click', deletePersonInfo);
+
+    function deletePersonInfo(e) {
+        let id = e.target.getAttribute('data-id');
+
+        fetch('http://localhost:3030/jsonstore/phonebook/' + id, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+
+        let trElement = e.target.parentElement.parentElement;
+        trElement.remove();
+    }
+
+    btnTdWrapper.appendChild(deleteBtn);
+    trEl.appendChild(nameTd);
+    trEl.appendChild(phoneTd);
+    trEl.appendChild(btnTdWrapper);
+
+    return trEl;
 }
 
 
