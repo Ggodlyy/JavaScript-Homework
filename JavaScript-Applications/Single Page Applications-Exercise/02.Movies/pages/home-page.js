4a2eaca231d2970d3c87bfe2e343634cf9bfc78b
Ggodlyy <nikogerenski@abv.bg>
2021-07-19 13:39:12 +0300

    Update

--- JavaScript-Applications/Single Page Applications-Exercise/02.Movies/pages/home-page.js
+++ JavaScript-Applications/Single Page Applications-Exercise/02.Movies/pages/home-page.js
@@ -1,12 +1,47 @@
 let main = null;
 let section = null;
+let container = null;
+
+async function getMovies() {
+    let res = await fetch('http://localhost:3030/data/movies');
+    let data = res.json();
+
+    return data;
+}
+
+function createMovieCard(movie) {
+    let el = document.createElement('div');
+    el.classList.add('mb-4');
+
+    el.innerHTML = ` <img class="card-img-top" src="${movie.img}"
+    alt="Card image cap" width="400">
+<div class="card-body">
+    <h4 class="card-title">${movie.title}</h4>
+</div>
+<div class="card-footer">  
+        <button id="${movie._id}" type="button" class="btn btn-info">Details</button>
+</div>`
+
+    return el;
+}
 
 export function setupHome(mainTarget, sectionTarget) {
     main = mainTarget;
     section = sectionTarget;
+    container = section.querySelector('.card-deck.d-flex.justify-content-center');
 }
 
 export async function showHome() {
+    container.innerHTML = 'Loading...';
     main.innerHTML = '';
     main.appendChild(section);
+
+    let movies = await getMovies();
+    let cards = movies.map(createMovieCard);
+
+    let fragment = document.createDocumentFragment();
+    cards.forEach(c => fragment.appendChild(c));
+
+    container.innerHTML = '';
+    container.appendChild(fragment);
 }
